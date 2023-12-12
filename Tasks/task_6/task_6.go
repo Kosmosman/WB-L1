cafb60473b9e3eab9db81b968415e29c69761260
joaquind <oninthebest@yandex.ru>
2023-12-12 23:44:41 +0300

    Complete task 6, need add comments

---
+++ Tasks/task_6/task_6.go
@@ -0,0 +1,65 @@
+// Реализовать все возможные способы остановки выполнения горутины.
+
+package main
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	stopChanel := make(chan struct{})
+	go GoroutineStopWithChan(stopChanel, &wg)
+	time.Sleep(2 * time.Second)
+	close(stopChanel)
+
+	var stopFlag atomic.Bool
+	go GoroutineStopWithFlag(&stopFlag, &wg)
+	time.Sleep(2 * time.Second)
+	stopFlag.Swap(true)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go GoroutineStopWithContext(ctx, &wg)
+	time.Sleep(2 * time.Second)
+	cancel()
+
+	wg.Wait()
+}
+
+func GoroutineStopWithChan(ch chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ch:
+			println("End goroutine with signal chanel")
+			return
+		default:
+			continue
+		}
+	}
+}
+
+func GoroutineStopWithContext(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			println("End goroutine with context done")
+			return
+		default:
+			continue
+		}
+	}
+}
+
+func GoroutineStopWithFlag(flag *atomic.Bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for !flag.Load() {
+	}
+	println("End goroutine with foreign flag")
+}
